7ec9dc79411733d894cb123b1cab41e09aebfe57
ART_List <artbdarm9@mail.ru>
2021-01-18 17:21:28 +0300

    first commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Artem Listopadov on 1/18/21.
+//  Copyright © 2021 Artem Listopadov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
