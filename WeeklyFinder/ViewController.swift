613242a96e62f1400e2dff6897191264038f81e5
ART_List <artbdarm9@mail.ru>
2021-01-18 17:55:06 +0300

    added interface

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func findDay(_ sender: UIButton) {
+    }
+    
 
 }
 
